dd0e4bf7bb0d10580e6be3bc76c2701875d5b1bf
Schiiss <conner.schiissler@gmail.com>
2019-08-03 16:45:44 -0600

    Create Profile Form

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import SelectListGroup from "../common/SelectListGroup";
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -12,10 +14,34 @@ class CreateProfile extends Component {
       bio: "",
       githubusername: "",
       movies: "",
-      books: ""
+      books: "",
+      errors: {}
     };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    console.log("submit");
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
+    const { errors } = this.state;
+
+    //Select options for interests
+    const options = [
+      { label: "* Select interests", value: 0 },
+      { label: "Action", value: "Action" },
+      { label: "Horror", value: "Horror" },
+      { label: "Documentaries", value: "Documentaries" }
+    ];
     return (
       <div className="create-profile">
         <div className="container">
@@ -26,6 +52,43 @@ class CreateProfile extends Component {
                 Let's get some information from you
               </p>
               <small className="d-block pb-3">* = required fields</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* Profile Handle"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for your profile."
+                />
+                <SelectListGroup
+                  placeholder="Interests"
+                  name="interests"
+                  value={this.state.interests}
+                  onChange={this.onChange}
+                  options={options}
+                  error={errors.interests}
+                  info="Give us an interest of yours"
+                />
+                <TextFieldGroup
+                  placeholder="Github Username"
+                  name="githubusername"
+                  value={this.state.githubusername}
+                  onChange={this.onChange}
+                  options={options}
+                  error={errors.githubusername}
+                  info="If you want your latest repos, include your github username"
+                />
+                <TextAreaFieldGroup
+                  placeholder="Bio"
+                  name="bio"
+                  value={this.state.bio}
+                  onChange={this.onChange}
+                  options={options}
+                  error={errors.bio}
+                  info="Tell us a little bit about yourself"
+                />
+              </form>
             </div>
           </div>
         </div>
@@ -44,4 +107,4 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(null)(CreateProfile);
+export default connect(mapStateToProps)(CreateProfile);
