a1466ab7c90f30843cb0fb7120754a3beaf40650
Schiiss <conner.schiissler@gmail.com>
2020-04-28 20:07:00 -0600

    created react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,26 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import ReactGA from "react-ga";
-import $ from "jquery";
-import "./App.css";
-import Header from "./Components/landing/Header";
-import Footer from "./Components/landing/Footer";
-import About from "./Components/landing/About";
-import Resume from "./Components/landing/Resume";
-import Portfolio from "./Components/landing/Portfolio";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      foo: "bar",
-      landingData: {},
-    };
-
-    ReactGA.initialize("UA-110570651-1");
-    ReactGA.pageview(window.location.pathname);
-  }
-
-  getLandingData() {
-    $.ajax({
-      url: "/landingData.json",
-      dataType: "json",
-      cache: false,
-      success: function (data) {
-        this.setState({ landingData: data });
-      }.bind(this),
-      error: function (xhr, status, err) {
-        console.log(err);
-        alert(err);
-      },
-    });
-  }
-
-  componentDidMount() {
-    this.getLandingData();
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Header data={this.state.landingData.main} />
-          <About data={this.state.landingData.main} />
-          <Resume data={this.state.landingData.resume} />
-          <Portfolio data={this.state.landingData.portfolio} />
-          <Footer data={this.state.landingData.main} />
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
