9f5b7918d6d7236755df835805e3083539ec1c43
Schiiss <conner.schiissler@gmail.com>
2020-05-02 14:17:21 -0600

    create-react-app and initial layout

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
+
+class Landing extends Component {
+  render() {
+    return (
+      <header id="home">
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
+
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="row banner">
+          <div className="banner-text">
+            <h1 className="responsive-headline">Welcome to the Home Portal</h1>
+            <h3>
+              A home automation tool to make everyday tasks easier <span></span>
+            </h3>
+            <hr />
+            <ul className="social"></ul>
+          </div>
+        </div>
+
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
+    );
+  }
+}
+
+export default Landing;
